46687f6bb44c44cf7def5047c826cec7a316cd06
Behnder <mercado.nicolas@live.com>
2022-04-29 05:39:53 -0300

    first commit

---
+++ Assets/Scripts/Main/Menu.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+
+    public const int MAX_PINES = 10;
+  
+
+    public List<List<int>> rounds = new List<List<int>>();
+
+
+    private void Awake()
+    {
+        Game();
+
+        foreach (var item in rounds)
+        {
+            print("Total " + item[0]);
+            print("Primera " + item[1]);
+            print("Segunda " + item[2]);
+            print("------------------");
+
+        }
+    }
+
+
+    public int FallenPines(int maxPines)
+    {
+
+        return Random.Range(0, maxPines + 1);
+
+
+    }
+
+    public void Game()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            rounds.Add(PlayOneRound());
+            if (i > 0)
+            {
+                if (IsThisRoundStrike(rounds[i - 1]))
+                {
+                    SumWhenIsStrike(i);
+                }
+                else if (IsThisRoundSpare(rounds[i - 1]))
+                {
+                    SumWhenIsSpare(i);
+                }
+
+            }
+        }
+        CheckBonus();
+    }
+
+    public void CheckBonus()
+    {
+        var totalBonificacion = 0;
+        if (IsThisRoundStrike(rounds[rounds.Count-1]))
+        {
+
+            totalBonificacion = PlayOneRound()[0];
+        
+        }
+        else if (IsThisRoundSpare(rounds[rounds.Count-1]))
+        {
+            totalBonificacion = PlayOneRound()[1];
+        }
+
+        rounds[rounds.Count - 1][0] += totalBonificacion;
+    }
+
+    public int GetTotalScore(List<List<int>> completlyGame)
+    {
+        var score = 0;
+
+        foreach (var round in completlyGame)
+        {
+            score += round[0];
+        }
+        return score;
+
+     
+    }
+
+    public bool IsThisRoundStrike(List<int> round)
+    {
+
+        if (round[1] == 10)
+        {
+
+            return true;
+        }
+        return false;
+
+    }
+
+    public int SumWhenIsStrike(int i)
+    {
+        return rounds[i - 1][0] += rounds[i][1] + rounds[i][2];
+    }
+
+    public int SumWhenIsSpare(int i)
+    {
+        return rounds[i - 1][0] += rounds[i][1];
+    }
+
+    public bool IsThisRoundSpare(List<int> gameWithSpare)
+    {
+
+        if (gameWithSpare[1] < 10 && gameWithSpare[1] + gameWithSpare[2] == 10)
+        {
+            return true;
+        }
+
+
+        return false;
+    }
+
+    public int GetScoreInRound(List<int> oneRound)
+    {
+        return (oneRound[1] + oneRound[2]);
+    }
+
+    public List<int> PlayOneRound()
+    {
+
+        List<int> oneRound = new List<int>();
+        int PinesFallenFirstTry = 0, PinesFallenSecondTry = 0;
+
+        PinesFallenFirstTry = FallenPines(MAX_PINES);
+        PinesFallenSecondTry = FallenPines(MAX_PINES - PinesFallenFirstTry);
+
+
+        oneRound.Add(0);
+        oneRound.Add(PinesFallenFirstTry);
+        oneRound.Add(PinesFallenSecondTry);
+        oneRound[0] = GetScoreInRound(oneRound);
+
+        return oneRound;
+
+    }
+
+}
---
+++ Assets/Scripts/Tests/TestEditMode/TestKataBowling.cs
@@ -0,0 +1,170 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    public class TestKataBowling
+    {
+        private Menu menu;
+
+        [SetUp]
+        public void Setup()
+        {
+            menu = new Menu();
+        }
+
+
+        [Test]
+        public void Should_throw_more_than_one_and_less_than_eleven()
+        {
+            //Arrrange
+
+            //Act
+            var bowlTh = menu.FallenPines(10);//6
+            bool isTrue = (bowlTh >= 0 && bowlTh <= 10);
+            //Assert
+            Assert.AreEqual(true, isTrue);
+
+        }
+        [Test]
+        public void Should_be_score_zero_when_always_fail()
+        {
+
+            //Arrange
+            List<List<int>> CompletlyGame = new List<List<int>>
+                            {   new List<int>(){ 0,0,0},
+                                new List<int>(){ 0,0,0},
+                                new List<int>(){ 0,0,0},
+                                new List<int>(){ 0,0,0},
+                                new List<int>(){ 0,0,0},
+                                new List<int>() { 0, 0, 0 },
+                                new List<int>() { 0, 0, 0 },
+                                new List<int>() { 0, 0, 0 },
+                                new List<int>(){ 0, 0, 0 },
+                                new List<int>(){0,0,0}
+                            };
+
+            //Act
+            int totalScore = menu.GetTotalScore(CompletlyGame);
+            //asset
+            Assert.AreEqual(0, totalScore);
+            
+        }
+        [Test]
+        public void Should_be_add_points_when_not_have_strike_or_spare()
+        {
+
+            //Arrange
+            List<List<int>> CompletlyGame = new List<List<int>>
+                            {   new List<int>(){ 5,2,3},
+                                new List<int>(){ 3,1,2},
+                                new List<int>(){ 9,4,5},
+                                new List<int>(){ 8,4,4},
+                                new List<int>(){ 7,3,4},
+                                new List<int>(){ 6, 3, 3 },
+                                new List<int>(){ 6, 1, 5 },
+                                new List<int>(){ 7, 1, 6 },
+                                new List<int>(){ 4, 4, 0 },
+                                new List<int>(){1,0,1}
+                            };
+
+            //Act
+            int totalScore = menu.GetTotalScore(CompletlyGame);
+            //asset
+            Assert.AreEqual(56, totalScore);
+        }
+        [Test]
+        public void Should_add_points_to_the_last_round_when_last_round_was_strike()
+        {
+
+            //Arrange
+            List<List<int>> GameWithStrike = new List<List<int>>
+                            {  
+                                new List<int>(){ 10,10,0},
+                                new List<int>(){ 4,2,2},
+                            };
+
+            //Act
+             menu.rounds = GameWithStrike;
+             var scoreRound= menu.SumWhenIsStrike(1);
+
+            //asset
+            Assert.AreEqual(14, scoreRound);
+        }
+
+        [Test]
+        public void Should_add_points_to_the_last_round_when_last_round_was_spare()
+        {
+
+            //Arrange
+            List<List<int>> GameWithSpare = new List<List<int>>
+                            {
+                                new List<int>(){ 10,6,4},
+                                new List<int>(){ 4,2,2},
+                            };
+
+            menu.rounds = GameWithSpare;
+
+            //Act
+            var scoreRound = menu.SumWhenIsSpare(1);
+
+            //asset
+            Assert.AreEqual(12, scoreRound);
+        }
+
+        [Test]
+        public void Should_be_strike_when_ten_pines_are_fallen_at_first_turn()
+        {
+
+            //Arrange
+            List<int> GameWithStrike = new List<int> { 10, 10, 0 };
+            //Act
+            bool isStrike = menu.IsThisRoundStrike(GameWithStrike);
+
+            //asset
+            Assert.AreEqual(true, isStrike);
+        }
+
+        [Test]
+        public void Should_be_spare_when_ten_pines_are_fallen_in_total_and_is_not_strike()
+        {
+
+            //Arrange
+            List<int> GameWithSpare= new List<int> { 10, 1, 9 };
+            //Act
+            bool isSpare = menu.IsThisRoundSpare(GameWithSpare);
+
+            //asset
+            Assert.AreEqual(true, isSpare);
+        }
+
+        [Test]
+        public void Should_be_add_points_when_not_have_strike_or_spare_in_one_round()
+        {
+
+            //Arrange
+            List < int> oneRound= new List<int>() { 5, 2, 3 };
+
+            //Act
+            int totalScoreOfRound = menu.GetScoreInRound(oneRound);
+            //asset
+            Assert.AreEqual(5, totalScoreOfRound);
+        }
+
+        [Test]
+        
+        public void Should_be_add_two_more_rounds_when_last_round_is_strike()
+        {
+
+            //Arrange
+            List<int> oneRound = new List<int>() { 5, 2, 3 };
+
+            //Act
+            int totalScoreOfRound = menu.GetScoreInRound(oneRound);
+            //asset
+            Assert.AreEqual(5, totalScoreOfRound);
+        }
+
+
+    }
+}
